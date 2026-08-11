4b40bd405e2ab7928f3914ba9fd1b50ca4acd5ee
awallace77 <andrew77wallace@outlook.com>
2026-08-11 00:46:40 -0400

    Added missing page

---
+++ app/top-21/page.tsx
@@ -0,0 +1,17 @@
+import Link from "next/link";
+
+export default function Top21Page() {
+  return (
+    <main className="min-h-screen bg-[#f3efe6] px-6 py-10 text-[#171512] md:px-12 pt-28">
+      <div className="mx-auto max-w-7xl">
+        <Link
+          href="/"
+          className="text-sm uppercase tracking-[0.2em] opacity-60 hover:opacity-100"
+        >
+          ← Fred&apos;s Records
+        </Link>
+        TODO: Add top21 page content
+      </div>
+    </main>
+  );
+}
